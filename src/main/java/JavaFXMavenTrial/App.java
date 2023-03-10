3f642227f04918b06a57e03d4e8a853a50677bd4
Xrayya <nur.alkhiyar@gmail.com>
2023-03-10 22:00:18 +0700

    add all

--- src/main/java/JavaFXMavenTrial/App.java
+++ src/main/java/JavaFXMavenTrial/App.java
@@ -1,11 +1,16 @@
 package JavaFXMavenTrial;
 
+import JavaFXMavenTrial.Components.TransformRect;
 import javafx.application.Application;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
 
 /**
  * JavaFX App
@@ -16,10 +21,14 @@ public class App extends Application {
     public void start(Stage stage) {
         var javaVersion = SystemInfo.javaVersion();
         var javafxVersion = SystemInfo.javafxVersion();
-
         var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
-        stage.setScene(scene);
+        var jfxLabelScene = new Scene(new StackPane(label), 640, 480);
+
+        var helloScene = new Scene(sayHello(), 100, 100);
+
+        var rectScene = new Scene(TransformRect.createRect(), 300, 300);
+
+        stage.setScene(rectScene);
         stage.show();
     }
 
@@ -27,4 +36,7 @@ public class App extends Application {
         launch();
     }
 
+    private Parent sayHello() {
+        return new StackPane(new Text("Hello"));
+    }
 }
